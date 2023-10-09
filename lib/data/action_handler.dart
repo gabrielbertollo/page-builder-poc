2a355ac96eea6875c5102c1d02cb68b02b7fb882
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2023-10-09 14:57:23 -0300

    feat: add action handler class

---
+++ lib/data/action_handler.dart
@@ -0,0 +1,47 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+import '../widgets/modal_component.dart';
+
+enum Action {
+  modal,
+  navigation;
+
+  static Action? fromString(String action) {
+    switch (action) {
+      case 'modal':
+        return Action.modal;
+      case 'navigation':
+        return Action.navigation;
+      default:
+        return null;
+    }
+  }
+}
+
+class ActionHandler {
+  static void handleAction(Map<String, dynamic> data, BuildContext context) {
+    final action = Action.fromString(data['type']);
+    final child = data['child'];
+
+    switch (action) {
+      case Action.modal:
+        showDialog(
+          context: context,
+          builder: (context) {
+            return ModalComponent(
+              title: child['title'],
+              description: child['description'],
+            );
+          },
+        );
+        break;
+      case Action.navigation:
+        log('Navigate to ${data['route']}');
+        break;
+      default:
+        log('Unknown action');
+    }
+  }
+}
