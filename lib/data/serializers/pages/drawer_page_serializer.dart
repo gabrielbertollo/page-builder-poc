593adec3d9577a5385a20ba210a4e89ed613fe98
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2023-10-10 15:49:42 -0300

    fix(drawer-page): null safety error

--- lib/data/serializers/pages/drawer_page_serializer.dart
+++ lib/data/serializers/pages/drawer_page_serializer.dart
@@ -7,7 +7,8 @@ class DrawerPageSerializer {
   static Widget fromJson(Map<String, dynamic> json) {
     return DrawerPage(
       title: PageBuilder.fromJson(json['title']),
-      drawer: PageBuilder.fromJson(json['drawer']),
+      drawer:
+          json['drawer'] != null ? PageBuilder.fromJson(json['drawer']) : null,
       body: PageBuilder.fromJson(json['body']),
     );
   }
