c20e0c805afcf269b16910378cd0d1bba730db79
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2023-10-11 10:27:38 -0300

    feat(main): move navigation args to route builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'data/serializers/page_builder.dart';
 import 'home.dart';
 
 void main() {
@@ -19,7 +20,15 @@ class MainApp extends StatelessWidget {
       /// When using this implementation as a package, the routes should not
       /// be present here, but in the app that uses this package.
       routes: {
-        '/builder': (context) => const Home(),
+        '/builder': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as Map?;
+          return Home(
+            requestUrl: args?['requestUrl'],
+            skeleton: args?['skeleton'] != null
+                ? PageBuilder.fromJson(args?['skeleton'])
+                : null,
+          );
+        },
       },
       home: const Home(),
     );
