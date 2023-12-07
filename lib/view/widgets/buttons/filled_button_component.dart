0ad1f62539e0b8f040a30b52a17505ef5635a76d
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2023-12-07 14:33:10 -0300

    feat(filled-button): refactor action call and widget render

--- lib/view/widgets/buttons/filled_button_component.dart
+++ lib/view/widgets/buttons/filled_button_component.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:ui';
+import 'dart:math' as math;
 
+import '../../../data/entities/action.dart';
+import '../../../home.dart';
 import 'button.dart';
 
 class FilledButtonComponent extends Button {
@@ -13,31 +18,72 @@ class FilledButtonComponent extends Button {
     super.key,
   });
 
+  Widget child(BuildContext context, bool isLoading) {
+    if (isLoading) {
+      return const SizedBox(
+        height: 16,
+        width: 16,
+        child: CircularProgressIndicator(strokeWidth: 2.5),
+      );
+    }
+
+    if (icon != null) {
+      final double scale = MediaQuery.textScaleFactorOf(context);
+      // Adjust the gap based on the text scale factor. Start at 8, and lerp
+      // to 4 based on how large the text is.
+      final double gap =
+          scale <= 1 ? 8 : lerpDouble(8, 4, math.min(scale - 1, 1))!;
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.network(icon!),
+          SizedBox(width: gap),
+          Flexible(child: Text(label!))
+        ],
+      );
+    }
+
+    return Text(label!);
+  }
+
+  void onPressed(
+    BuildContext context,
+    ValueNotifier<bool> isLoadingListenable,
+  ) {
+    if (action is SubmitAction) {
+      (action as SubmitAction).handleInteraction(
+        context: context,
+        notifier: isLoadingListenable,
+        onSuccess: () {
+          Navigator.of(context).pop();
+          context.read<HomeProvider>().refresh();
+        },
+      );
+      return;
+    }
+    action!.handleInteraction(context: context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (icon != null) {
-      if (tonal) {
-        return FilledButton.tonalIcon(
-          icon: Image.network(icon!),
-          label: Text(label!),
-          onPressed: () => action!.handleInteraction(context: context),
+    final isLoadingListenable = ValueNotifier<bool>(false);
+    return ValueListenableBuilder(
+      valueListenable: isLoadingListenable,
+      builder: (_, isLoading, widget) {
+        if (tonal) {
+          return FilledButton.tonal(
+            onPressed: isLoading
+                ? null
+                : () => onPressed(context, isLoadingListenable),
+            child: child(context, isLoading),
+          );
+        }
+        return FilledButton(
+          onPressed:
+              isLoading ? null : () => onPressed(context, isLoadingListenable),
+          child: child(context, isLoading),
         );
-      }
-      return FilledButton.icon(
-        icon: Image.network(icon!),
-        label: Text(label!),
-        onPressed: () => action!.handleInteraction(context: context),
-      );
-    }
-    if (tonal) {
-      return FilledButton.tonal(
-        child: Text(label!),
-        onPressed: () => action!.handleInteraction(context: context),
-      );
-    }
-    return FilledButton(
-      child: Text(label!),
-      onPressed: () => action!.handleInteraction(context: context),
+      },
     );
   }
 }
